532dda41e088a5b714b874c86877d3a5ce3011e3
Yuriy Buranov <Yura962Bur@mail.ru>
2021-09-17 13:48:48 +0400

    feat: fix bug in script for menu items

--- dev/js/script.js
+++ dev/js/script.js
@@ -28,9 +28,11 @@ $(window).on('load', () => {
     menuItem.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.remove('hamburger--active');
-            menuText.classList.toggle('d-block'),
-            menuPhone.classList.toggle('d-none');
             menu.classList.remove('menu--active');
+            if($(window).width() < 992){
+                menuText.classList.toggle('d-block'),
+                menuPhone.classList.toggle('d-none');
+            }
         });
     });
 
@@ -59,8 +61,6 @@ $(window).on('load', () => {
             '</svg>' +
         '</button>'
     });
-
-    
 
     // Отправка формы
     // $("form").on("submit", (function (e) {
@@ -110,3 +110,8 @@ function showYaMaps(){
     script.src   = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A32062da92eafd2a9f620ab4b66e0726df9b6520bcd7502c0ff743d1ad9434ff3&amp;width=100%25&amp;height=100%&amp;lang=ru_RU&amp;scroll=true";
     document.getElementById("YaMaps").appendChild(script);
 }
+
+
+/* var greenBlock = window.getComputedStyle(document.querySelector('.longlife'), ':before').height;
+
+console.log(greenBlock); */
